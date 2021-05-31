e5f3cd1662efa86152e4691699014f96227f09b2
AntonPodash <apodash01@gmail.com>
2021-06-01 01:05:28 +0300

    lab_3.3

---
+++ lab_3.3/App.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
+import Genetic from './Genetic'
+
+export default function App() {
+  const [a, setA] = useState(null);
+  const [b, setB] = useState(null);
+  const [c, setC] = useState(null);
+  const [d, setD] = useState(null);
+  const [y, setY] = useState(null);
+  const [result, setResult] = useState('[]');
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <TextInput style={styles.expression}
+          onChangeText={setA}
+          value={a}
+          placeholder="a"
+          keyboardType="numeric"
+        />
+        <Text style={styles.expression}>{'*x1 + '}</Text>
+        <TextInput style={styles.expression}
+          onChangeText={setB}
+          value={b}
+          placeholder="b"
+          keyboardType="numeric"
+        />
+        <Text style={styles.expression}>{'*x2 + '}</Text>
+        <TextInput style={styles.expression}
+          onChangeText={setC}
+          value={c}
+          placeholder="c"
+          keyboardType="numeric"
+        />
+        <Text style={styles.expression}>{'*x3 + '}</Text>
+        <TextInput style={styles.expression}
+          onChangeText={setD}
+          value={d}
+          placeholder="d"
+          keyboardType="numeric"
+        />
+        <Text style={styles.expression}>{'*x4 = '}</Text>
+        <TextInput style={styles.expression}
+          onChangeText={setY}
+          value={y}
+          placeholder="y"
+          keyboardType="numeric"
+        />
+      </View>
+      <Text style={styles.result}>
+        {`[x1, x2, x3, x4] = ${result}`}
+      </Text>
+      <View style={styles.btn}>
+        <Button
+          title="Calculate"
+          color="#fff"
+          onPress={() => setResult(new Genetic([a, b, c, d], y).solve())}
+        />
+      </View>
+
+    </SafeAreaView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    top: 200,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  expression: {
+    fontSize: 25
+  },
+  result: {
+    alignSelf: 'center',
+    top: 269,
+    fontSize: 25
+  },
+  time: {
+    alignSelf: 'center',
+    top: 320,
+    fontSize: 22
+  },
+  btn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    top: 400,
+    height: 50,
+    width: 150,
+    backgroundColor: '#9d9fe7',
+    borderRadius: 10,
+  },
+});
---
+++ lab_3.3/Genetic.js
@@ -0,0 +1,83 @@
+const random = (min, max) => ~~(Math.random() * (max - min) + min)
+
+class Chromosome {
+    genes = [];
+    fitness = Infinity;
+    task = [];
+    target = 0;
+
+    calc = () =>
+        this.genes.reduce((a, gene, i) => a + (gene * this.task[i]))
+
+    constructor({ genes, task, target }) {
+        Object.assign(this, { genes, task, target });
+        this.calcFitness();
+    }
+
+    crossover(partner) {
+        const child = this.clone();
+        const fromParent = child.genes.length / 2;
+        child.genes = [...child.genes.slice(0, fromParent), ...partner.genes.slice(child.genes.length - fromParent)];
+        child.calcFitness();
+
+        return child
+    }
+
+    calcFitness() {
+        this.fitness = Math.abs(this.target - this.calc());
+    }
+
+    clone() {
+        return Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+    }
+}
+
+let start = performance.now();
+class Genetic {
+    population = []
+    constructor(task, target) {
+        const { length } = task
+        this.population =
+            Array.from(
+                { length: length + 1 },
+                () => new Chromosome({
+                    genes: Array.from({ length }, () => random(1, target / 2)),
+                    task: task,
+                    target: target,
+                })
+            )
+    }
+
+    solve() {
+        while (true) {
+            const chromosome = this.crossover()
+            if (chromosome)
+                return chromosome.genes
+        }
+    }
+
+    crossover() {
+        const children = []
+        for (let i = 0; i < this.population.length; i++) {
+            const parents = this.population
+                .map((chromosome) => ({ chromosome, probability: Math.random() * (chromosome.fitness * 1000) }))
+                .sort((a, b) => a.probability - b.probability)
+
+            const parent = parents[0].chromosome
+            const partner = parents[1].chromosome
+            const child = parent.crossover(partner)
+
+            if (child.fitness === 0)
+                return child
+
+            children.push(child)
+        }
+
+        this.population = children
+    }
+}
+let end = performance.now()
+let res = start - end;
+console.log(res);
+
+export default Genetic;
